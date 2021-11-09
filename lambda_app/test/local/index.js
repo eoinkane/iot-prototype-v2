4543dbdd09c7802216de73a53aaefc38ec7c86f7
eoinkane <eoinkane23@gmail.com>
2021-11-09 12:43:26 +0000

    Updated local testing script to allow custom body & path

--- lambda_app/test/local/index.js
+++ lambda_app/test/local/index.js
@@ -1,5 +1,8 @@
 import { handler } from "../../src/app.js";
 import event from "./data.js";
+
 (async () => {
+    event.path = process.argv[2] ? process.argv[2] : "/card/on-site/001";
+    event.body = process.argv[3] ? process.argv[3] : "[]";
     console.log(await handler(event));
 })()
