cdae1b167f46e174162de25855ae6ec3a8e5b540
eoinkane <eoinkane23@gmail.com>
2021-11-09 12:57:59 +0000

    Removed comments

--- lambda_app/src/app.js
+++ lambda_app/src/app.js
@@ -3,14 +3,11 @@ import { getCardOnSiteByCardId } from './paths/index.js';
 
 export const handler = lambdaRouterHandler({
     proxyIntegration: {
-        // assumes the first path part is `stage` and removes it.
         removeBasePath: true,
         routes: [
             {
-                // request-path-pattern with a path variable:
                 path: '/card/on-site/{cardId}',
                 method: 'GET',
-                // we can use the path param 'cardId' in the action call:
                 action: getCardOnSiteByCardId
             }
         ]
