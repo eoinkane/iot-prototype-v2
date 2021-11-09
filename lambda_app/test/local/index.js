46fb8e8f46ce3c78271ef3ae3a9e574f95186505
eoinkane <eoinkane23@gmail.com>
2021-11-09 12:56:26 +0000

    Updated output display of local test script

--- lambda_app/test/local/index.js
+++ lambda_app/test/local/index.js
@@ -4,5 +4,7 @@ import event from "./data.js";
 (async () => {
     event.path = process.argv[2] ? process.argv[2] : "/card/on-site/001";
     event.body = process.argv[3] ? process.argv[3] : "[]";
-    console.log(await handler(event));
+    const proxyResult = await handler(event);
+    console.log(proxyResult);
+    console.log(JSON.parse(proxyResult.body));
 })()
