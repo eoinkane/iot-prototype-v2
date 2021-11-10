67cd93b493bfabed397b870a2588d58b89a20eb0
eoinkane <eoinkane23@gmail.com>
2021-11-10 04:05:17 +0000

    particle code

---
+++ particle/v1/app/iot-prototype-v1.ino
@@ -0,0 +1,220 @@
+/*
+ * Initial Author: ryand1011 (https://github.com/ryand1011)
+ *
+ * Reads data written by a program such as "rfid_write_personal_data.ino"
+ *
+ * See: https://github.com/miguelbalboa/rfid/tree/master/examples/rfid_write_personal_data
+ *
+ * Uses MIFARE RFID card using RFID-RC522 reader
+ * Uses MFRC522 - Library
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+*/
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <set>
+
+constexpr uint8_t RST_PIN = 8;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 7;     // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+// Define the pins we're going to call pinMode on
+int redLed = D3; // This one is the red led
+int greenLed = D4; // This one is the green led
+int inputpin = D2;	//used to read the Pushbutton
+
+//*****************************************************************************************//
+void setup() {
+  Serial.begin(9600);                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
+  
+  // Initialize D0 + D7 pin as output
+  // It's important you do this here, inside the setup() function rather than outside it or in the loop function.
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(inputpin, INPUT);
+}
+
+
+/**
+ * Helper function to flash red led.
+ */
+void flash_red_led() {
+    Serial.println("DEBUG: Flashing RED led at pin (redLed) D3 for 5 seconds (5000 ms).\n");
+    
+    digitalWrite(redLed, HIGH); // turn on the led
+    delay(5000);  // wait 5 seconds
+    digitalWrite(redLed, LOW); // turn off the led
+}
+
+/**
+ * Helper function to flash green led.
+ */
+void flash_green_led() {
+    Serial.println("DEBUG: Flashing GREEN led at pin (greenLed) D4 for 5 seconds (5000 ms).\n");
+    
+    digitalWrite(greenLed, HIGH); // turn on the led
+    delay(5000);  // wait 5 seconds
+    digitalWrite(greenLed, LOW); // turn off the led
+}
+
+
+
+/**
+ * Helper function to construct 3 character string from RFID PICC buffer.
+ */
+String id_buffer_to_string(byte* idBuffer)
+{
+  byte tes[4];
+  char str[4];
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    if (idBuffer[i] != 32 && idBuffer[i] != 239 && idBuffer[i] != 191 && idBuffer[i] != 189)
+    {
+      tes[i] = idBuffer[i];
+    }
+    if (i == 3)
+    {
+        break;
+    }
+  }
+  // Copy contents
+  memcpy(str, tes, 4);
+  // Append NULL terminator
+  str[sizeof(tes)] = '\0';
+  
+  String nana(str);
+  
+  return nana;
+}
+
+
+//*****************************************************************************************//
+void loop() {
+
+  // Set up test data for the prototype
+  std::set<std::string> g1;
+
+  for (int i = 1; i <= 5; i++)
+    g1.insert("00" + std::to_string(i));
+    
+  if(digitalRead(inputpin) == HIGH){
+    Serial.println("Button pressed");
+    flash_red_led();
+    delay(2000);
+    flash_green_led();
+  }
+
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  //-------------------------------------------
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Card Detected:**"));
+
+  //-------------------------------------------
+
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
+
+  //-------------------------------------------
+
+  byte buffer1[18];
+
+  block = 4;
+  len = 18;
+
+  //------------------------------------------- GET STAFF ID
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    flash_red_led();
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    flash_red_led();
+    return;
+  }
+
+  //---------------------------------------- GET JOB
+
+  byte buffer2[18];
+  block = 1;
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    flash_red_led();
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    flash_red_led();
+    return;
+  }
+  //----------------------------------------
+
+  String s1 = id_buffer_to_string(buffer1);
+  Serial.println("Card ID: " + s1);
+  
+  String s2 = id_buffer_to_string(buffer2);
+  Serial.println("Staff ID: " + s2);
+  
+  // ---------- logic section
+  
+  if (g1.find(s1.c_str()) != g1.end())
+  {
+    Serial.println("\nAccess Granted");
+    flash_green_led();
+    
+  } else {
+    Serial.println("\nAccess Denied");
+    flash_red_led();
+  }
+  
+  Serial.println(F("\n**End Reading**\n"));
+
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
+//*****************************************************************************************//
---
+++ particle/v2/write-card2/iot-prototype-v2-write-card2.ino
@@ -0,0 +1,162 @@
+/*
+ * Write personal data of a MIFARE RFID card using a RFID-RC522 reader
+ * Uses MFRC522 - Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT. 
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ * Hardware required:
+ * Arduino
+ * PCD (Proximity Coupling Device): NXP MFRC522 Contactless Reader IC
+ * PICC (Proximity Integrated Circuit Card): A card or tag using the ISO 14443A interface, eg Mifare or NTAG203.
+ * The reader can be found on eBay for around 5 dollars. Search for "mf-rc522" on ebay.com. 
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 8;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 7;     // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);        // Initialize serial communications with the PC
+  SPI.begin();               // Init SPI bus
+  mfrc522.PCD_Init();        // Init MFRC522 card
+  Serial.println(F("Write personal data on a MIFARE PICC "));
+}
+
+// to decide what card is being written to
+int counter = 0;
+
+void loop() {
+
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print(F("Card UID:"));    //Dump UID
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.print(F(" PICC type: "));   // Dump PICC type
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+  
+  // start of write
+
+  byte block;
+  MFRC522::StatusCode status;
+  byte len = 30;
+
+
+  block = 1;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PRE:91:PCD_Authenticate() success: "));
+
+  byte blockDataStaffIdOnline[34] = {"002"};
+  for (byte i = len; i < 30; i++) blockDataStaffIdOnline[i] = ' ';
+  // Write block
+  status = mfrc522.MIFARE_Write(block, blockDataStaffIdOnline, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PRE:100:MIFARE_Write() success: "));
+
+  block = 2;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  // Write block
+  status = mfrc522.MIFARE_Write(block, &blockDataStaffIdOnline[16], 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PRE:118:MIFARE_Write() success: "));
+// -------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------
+  block = 4;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  
+  byte blockDataCardIdOnline[34] = {"002"};
+  for (byte i = len; i < 30; i++) blockDataCardIdOnline[i] = ' ';
+
+  // Write block
+  status = mfrc522.MIFARE_Write(block, blockDataCardIdOnline, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PRE:141:MIFARE_Write() success: "));
+
+  block = 5;
+  //Serial.println(F("Authenticating using key A..."));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PRE:147:PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  // Write block
+  status = mfrc522.MIFARE_Write(block, &blockDataCardIdOnline[16], 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else Serial.println(F("PRE:159:MIFARE_Write() success: "));
+  
+  // end of write
+  
+  Serial.println("Authorised Card written. Staff ID: 002 Card ID: 002");
+
+
+  Serial.println(" ");
+  mfrc522.PICC_HaltA(); // Halt PICC
+  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
+
+}
