79c63cfcbd89433377280ef129d6f8613da72490
eoinkane <eoinkane23@gmail.com>
2021-11-17 18:13:07 +0000

    Translating DB IDs to JSON IDs. The mysql libary was automatically casting the ids as INTs without the zerofil. Now translating them as strings so that they can be padded.

--- lambda_app/src/paths/getStaff.ts
+++ lambda_app/src/paths/getStaff.ts
@@ -9,11 +9,19 @@ export const getStaff = async (request, context) => {
   const staff = await conn.query('SELECT * FROM staff;');
 
   const body = (staff[0] as RowDataPacket[]).map((cur) => {
-    const staffId = cur['staff_id'];
+    let staffId = cur['staff_id'];
+    while (staffId.toString().length !== 3) {
+        staffId = '0' + staffId.toString();
+    }
+
     const firstName = cur['first_name'];
     const lastName = cur['last_name'];
     const accessPermission = cur['access_permission'];
-    const currentCardId = cur['current_card_id'];
+    let currentCardId = cur['current_card_id'];
+    while (currentCardId.toString().length !== 3) {
+        currentCardId = '0' + currentCardId.toString();
+    }
+
     return {
       staffId,
       firstName,
