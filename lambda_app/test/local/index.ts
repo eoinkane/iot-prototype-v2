e99558ba615cf4ccc3ba84efeccc39d5818b86bf
eoinkane <eoinkane23@gmail.com>
2021-11-17 11:51:39 +0000

    Addressed TS compile errors so that ts-node can be used

--- lambda_app/test/local/index.ts
+++ lambda_app/test/local/index.ts
@@ -1,3 +1,4 @@
+import { RouterEvent } from 'aws-lambda-router';
 import { handler } from "../../src/index";
 import event from "./data";
 
@@ -5,7 +6,7 @@ import event from "./data";
     event.path = process.argv[2] ? process.argv[2] : "/card/on-site/001";
     event.body = process.argv[3] ? process.argv[3] : "[]";
     event.httpMethod = process.argv[3] ? "POST" : "GET";
-    const proxyResult = await handler(event);
+    const proxyResult = await handler((event as unknown as RouterEvent), null);
     console.log(proxyResult);
     console.log(JSON.parse(proxyResult.body));
 })();
